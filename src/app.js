a2628fc151774f909063b308a8a043b76888b6ef
anafipsilva <126426081+anafipsilva@users.noreply.github.com>
2023-07-13 13:49:27 +0100

    added forecast - api integration

--- src/app.js
+++ src/app.js
@@ -44,29 +44,58 @@ function setCurrentDay() {
   h3.innerHTML = `${day}, ${currentDate} ${month} ${year}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-  const forecastElement = document.querySelector("#forecast");
-  console.log({forecastElement})
-  const days = [ "Wed"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  const forecastHTML = `
-    <div class="row">
-      ${days.map((day) => `
-        <div class="col text-center weather-day">
-          <div><strong>${day}</strong></div>
-            <svg class="mt-2" width="100" height="100" viewBox="0 0 32 32" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-              d="M15 5V2H17V5H15ZM20.634 5.97381L22.134 3.37573L23.8661 4.37573L22.3661 6.97381L20.634 5.97381ZM16 23C19.866 23 23 19.866 23 16C23 12.134 19.866 9 16 9C12.134 9 9 12.134 9 16C9 19.866 12.134 23 16 23ZM16 25C20.9706 25 25 20.9706 25 16C25 11.0294 20.9706 7 16 7C11.0294 7 7 11.0294 7 16C7 20.9706 11.0294 25 16 25ZM27 15H30V17H27V15ZM27.6243 8.13397L25.0263 9.63397L26.0263 11.366L28.6243 9.86603L27.6243 8.13397ZM8.13397 4.37573L9.63397 6.97381L11.366 5.97381L9.86603 3.37573L8.13397 4.37573ZM5.97375 11.366L3.37567 9.86603L4.37567 8.13397L6.97375 9.63397L5.97375 11.366ZM15 27V30H17V27H15ZM5 15H2V17H5V15ZM3.37562 22.134L5.97369 20.634L6.97369 22.366L4.37562 23.866L3.37562 22.134ZM9.63404 25.0264L8.13404 27.6244L9.86609 28.6244L11.3661 26.0264L9.63404 25.0264ZM22.134 28.6244L20.634 26.0264L22.366 25.0264L23.866 27.6244L22.134 28.6244ZM25.0263 22.366L27.6244 23.866L28.6244 22.134L26.0263 20.634L25.0263 22.366Z"
-              fill="#a6a6a6" />
-            </svg>
-        </div>`
-      ).join('')}
-    </div>
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
   `;
+    }
+  });
 
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  
+}
+
+
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+  console.log(apiUrl);
 }
 
 function setUnit(unit) {
@@ -96,6 +125,9 @@ function currentTemperature(response) {
   document.querySelector("#weather-icon").src = getIconSrc(icon);
   document.querySelector("#wind-info").innerHTML = wind;
   document.querySelector("#humidity-info").innerHTML = humidity;
+
+  getForecast(response.data.coord);
+
 }
 
 function getPositionByCoordinates(position) {
@@ -125,7 +157,7 @@ function search(event) {
     h2.innerHTML = searchInput.value;
 
     getPositionByCityName(searchInput.value);
-    displayForecast()
+    
 
   }
 }
